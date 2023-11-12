f6e7f52c33638fe6081b5c6f9f27208f00c67e9b
Ethan Honey <ethanhoney@btinternet.com>
2023-11-12 18:08:15 +0000

    Cleaned freeze then train

--- model_routines/freeze_then_train.py
+++ model_routines/freeze_then_train.py
@@ -1,62 +1,64 @@
+import os
+import numpy as np
 import torch
 from torch import optim
-import numpy as np
-import os
+import csv
 
-from torchvision import transforms
-from torch.utils.data import DataLoader, ConcatDataset
-# make sure 'models' folder is in same directory for next import to run
-from data import datasets, trans # in same directory, could rewrite these .py files to be cleaner too
+from ..utils.model_utils import load_or_initialize_training, freeze_layers, make_dataloader, check_frozen, exp_decay_learning_rate, compute_loss
+from ..utils.general_utils import seg_to_one_hot_channels, disjoint_to_overlapping
 
-from utils import *
-
-def freeze_then_continue_training(data_dir, ckpt_A_path, max_epoch, frozen_layers, out_dir=None, save_interval=10, batch_size=1):
+def freeze_then_continue_training(data_dir, prev_ckpt_path, max_epoch, frozen_layers, out_dir=None, backup_interval=10, batch_size=1):
 
     # Set up directories and paths.
     if out_dir is None:
         out_dir = os.getcwd()
-    ckpt_B_path = os.path.join(out_dir, 'latest_ckpt.pth.tar')
-    saved_ckpts_B_dir = os.path.join(out_dir, 'saved_ckpts_B')
-    if not os.path.exists(saved_ckpts_B_dir):
-        os.makedirs(saved_ckpts_B_dir)
-        os.system(f'chmod a+rwx {saved_ckpts_B_dir}')
+    latest_ckpt_path = os.path.join(out_dir, 'latest_ckpt.pth.tar')
+    training_loss_path = os.path.join(out_dir, 'training_loss.csv')
+    backup_ckpts_dir = os.path.join(out_dir, 'new_backup_ckpts')
+    if not os.path.exists(backup_ckpts_dir):
+        os.makedirs(backup_ckpts_dir)
+        os.system(f'chmod a+rwx {backup_ckpts_dir}')
 
-    ## Previous training summary A
-    ## Continue training summary B
+    print(f"Loading model from {prev_ckpt_path}...")
+    checkpoint = torch.load(prev_ckpt_path)
 
-    print(f"Loading model from {ckpt_A_path}...")
-    checkpoint = torch.load(ckpt_A_path)
-
-    # epoch = checkpoint['epoch']
-    training_regions = checkpoint['training_regions']
-    model_str = checkpoint['model_str']
-    model = MODEL_STR_TO_FUNC[model_str]
-    model.load_state_dict(checkpoint['model_sd'])
-    loss_functs_str = checkpoint['loss_functs_str']
-    loss_functs = [LOSS_STR_TO_FUNC[l] for l in loss_functs_str]
+    model = checkpoint['model']
+    loss_functions = checkpoint['loss_functions']
     loss_weights = checkpoint['loss_weights']
-    
+    training_regions = checkpoint['training_regions']
     init_lr = checkpoint['init_lr']
     decay_rate = checkpoint['decay_rate']
+
+    epoch = checkpoint['epoch']
+    model_sd = checkpoint['model_sd']
+
+    model.load_state_dict(model_sd)
+
+    print("---------------------------------------------------")
+    print(f"PREVIOUS TRAINING SUMMARY")
+    print(f"Model: {model}")
+    print(f"Loss functions: {loss_functions}") 
+    print(f"Loss weights: {loss_weights}")
+    print(f"Training regions: {training_regions}")
+    print(f"Epochs trained: {epoch}")
+    print("---------------------------------------------------")
+    print("CONTINUING TRAINING SUMMARY")
+    print(f"Data directory: {data_dir}")
+    print(f"Trained model checkpoint path: {prev_ckpt_path}")
+    print(f"Initial learning rate: {init_lr}")
+    print(f"Max epochs: {max_epoch}")
+    print(f"Out directory: {out_dir}")
+    print(f"Decay rate: {decay_rate}")
+    print(f"Backup interval: {backup_interval}")
+    print(f"Batch size: {batch_size}")
+    print("---------------------------------------------------")
 
     optimizer = optim.Adam(model.parameters(), lr=init_lr, weight_decay=0, amsgrad=True)
 
     # Check if continuing training for first time or continuing from a saved checkpoint.
-    if not os.path.exists(ckpt_B_path):
-        epoch_start = 1
-        print('No continuation of training checkpoint found. Will start continuation of train from beginning.')
+    epoch_start = load_or_initialize_training(model, optimizer, latest_ckpt_path)
 
-        freeze_layers(model, frozen_layers)
-
-    else:
-        print("Continuation of training checkpoint found. Loading checkpoint...")
-        checkpoint = torch.load(ckpt_B_path)
-
-        epoch_start = checkpoint['epoch'] + 1
-        model.load_state_dict(checkpoint['model_sd'])
-        optimizer.load_state_dict(checkpoint['optim_sd'])
-
-        freeze_layers(model, frozen_layers) 
+    freeze_layers(model, frozen_layers)
 
     train_loader = make_dataloader(data_dir, shuffle=True, mode='train')
 
@@ -64,7 +66,8 @@ def freeze_then_continue_training(data_dir, ckpt_A_path, max_epoch, frozen_layer
     for epoch in range(epoch_start, max_epoch+1):
         print(f'Starting epoch {epoch}...')
 
-        if epoch < 5:
+        # Check model is frozen appropriately at start.
+        if epoch == epoch_start:
             check_frozen(model, frozen_layers)
 
         exp_decay_learning_rate(optimizer, epoch, init_lr, decay_rate)
@@ -75,22 +78,23 @@ def freeze_then_continue_training(data_dir, ckpt_A_path, max_epoch, frozen_layer
             model.train()
 
             # Move data to GPU.
-            imgs = [img.cuda() for img in imgs]
+            imgs = [img.cuda() for img in imgs] # img is B1HWD
             seg = seg.cuda()
 
             # Split segmentation into 3 channels.
             seg = seg_to_one_hot_channels(seg)
+            # seg is B3HWD - each channel is one-hot encoding of a disjoint region
 
             if training_regions == 'overlapping':
                 seg = disjoint_to_overlapping(seg)
+                # seg is B3HWD - each channel is one-hot encoding of an overlapping region
 
-            x_in = torch.cat(imgs, dim=1)
+            x_in = torch.cat(imgs, dim=1) # x_in is B4HWD
             output = model(x_in)
             output = output.float()
 
             # Compute weighted loss, summed across each region.
-
-            loss = compute_loss(output, seg, loss_functs, loss_weights)
+            loss = compute_loss(output, seg, loss_functions, loss_weights)
 
             optimizer.zero_grad()
             loss.backward()
@@ -100,8 +104,7 @@ def freeze_then_continue_training(data_dir, ckpt_A_path, max_epoch, frozen_layer
 
         # Compute, save and report loss from the epoch.
         average_epoch_loss = np.mean(losses_over_epoch)
-        with open(os.path.join(out_dir, 'loss_values.dat'), 'a') as f:
-            f.write(f'{epoch}, {average_epoch_loss}\n')
+        save_tloss_csv(training_loss_path, epoch, average_epoch_loss)
         print(f'Epoch {epoch} completed. Average loss = {average_epoch_loss:.4f}.')
 
         print('Saving model checkpoint...')
@@ -109,25 +112,31 @@ def freeze_then_continue_training(data_dir, ckpt_A_path, max_epoch, frozen_layer
             'epoch': epoch,
             'model_sd': model.state_dict(),
             'optim_sd': optimizer.state_dict(),
-            'model_str': model_str,
-            'training_regions': training_regions,
-            'loss_functs_str': loss_functs_str,
+            'model': model,
+            'loss_functions': loss_functions,
             'loss_weights': loss_weights,
             'init_lr': init_lr,
+            'training_regions': training_regions,
             'decay_rate': decay_rate
         }
-        torch.save(checkpoint, ckpt_B_path)
-        if epoch % save_interval == 0:
-            torch.save(checkpoint, os.path.join(saved_ckpts_B_dir, f'epoch{epoch}.pth.tar'))
+        torch.save(checkpoint, latest_ckpt_path)
+        if epoch % backup_interval == 0:
+            torch.save(checkpoint, os.path.join(backup_ckpts_dir, f'epoch{epoch}.pth.tar'))
         print('Checkpoint saved successfully.')
 
+def save_tloss_csv(pathname, epoch, tloss):
+    with open(pathname, mode='a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        if epoch == 1:
+            writer.writerow(['Epoch', 'Training Loss'])
+        writer.writerow([epoch, tloss])
 
 if __name__ == '__main__':
 
     data_dir = '/mmfs1/home/ehoney22/debug_data/train'
-    ckpt_A_path = '/mmfs1/home/ehoney22/debug/saved_ckpts/epoch20.pth.tar'
+    prev_ckpt_path = '/mmfs1/home/ehoney22/debug/saved_ckpts/epoch20.pth.tar'
     max_epoch = 20
     frozen_layers = ['Conv1', 'Conv2', 'Conv3', 'Conv4', 'Conv5', 'Conv6', 'Conv7']
     out_dir = '/mmfs1/home/ehoney22/debug/freeze_then_continue_training'
 
-    freeze_then_continue_training(data_dir, ckpt_A_path, max_epoch, frozen_layers, out_dir)
+    freeze_then_continue_training(data_dir, prev_ckpt_path, max_epoch, frozen_layers, out_dir=out_dir)
